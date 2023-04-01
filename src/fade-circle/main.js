ec2759defe6c65db64c4ef89cfcfa75d9a473e82
romaindasilva <romain.dasilva@lachouette.co>
2023-04-01 17:30:16 +0200

    ✨ Add fade-circle

---
+++ src/fade-circle/main.js
@@ -0,0 +1,19 @@
+var gl;
+
+let allPoints = [];
+
+function init() {
+  const canvas = document.getElementById("canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+
+  // Set the default value of the color buffer
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear the color buffer
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const point = new Point(0, 0, 100);
+  point.draw();
+}
+
+init();
